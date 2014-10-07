ad6bd8a20371a65a622562ad132434fff1f424a5
Fernando Paredes <nano@fdp.io>
2014-10-07 09:49:31 -0700

    Initial CriminalFragment class

---
+++ app/src/main/java/io/fdp/criminalintent/CrimeFragment.java
@@ -0,0 +1,59 @@
+package io.fdp.criminalintent;
+
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+    private EditText mTitleField;
+
+    public CrimeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        mTitleField = (EditText) view.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+                // left blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+                mCrime.setTitle(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // left blank
+            }
+        });
+        return view;
+    }
+
+
+}
