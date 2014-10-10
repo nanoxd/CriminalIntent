ee6128225bdc5fbd421eee88176bef399b0b6b91
Fernando Paredes <nano@fdp.io>
2014-10-09 20:56:51 -0700

    Don't focus on check box

--- app/src/main/java/io/fdp/criminalintent/CrimeListFragment.java
+++ app/src/main/java/io/fdp/criminalintent/CrimeListFragment.java
@@ -26,14 +26,13 @@ public class CrimeListFragment extends ListFragment {
         getActivity().setTitle(R.string.crimes_title);
         mCrimes = CrimeLab.get(getActivity()).getCrimes();
 
-        ArrayAdapter<Crime> adapter = new ArrayAdapter<Crime>(getActivity(),
-                android.R.layout.simple_list_item_1,  mCrimes);
+        CrimeAdapter adapter = new CrimeAdapter(mCrimes);
         setListAdapter(adapter);
     }
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
-        Crime crime = (Crime) (getListAdapter()).getItem(position);
+        Crime crime = ((CrimeAdapter) getListAdapter()).getItem(position);
         Log.d(TAG, crime.getTitle() + " was clicked");
     }
 
