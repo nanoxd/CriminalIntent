f70ad6ce73707b9923a5f5c5fb324eb6c6b0cb7b
Fernando Paredes <nano@fdp.io>
2014-10-09 20:53:27 -0700

    Add custom CrimeAdapter to use custom list view

--- app/src/main/java/io/fdp/criminalintent/CrimeListFragment.java
+++ app/src/main/java/io/fdp/criminalintent/CrimeListFragment.java
@@ -4,8 +4,11 @@ import android.app.ListFragment;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -32,5 +35,35 @@ public class CrimeListFragment extends ListFragment {
     public void onListItemClick(ListView l, View v, int position, long id) {
         Crime crime = (Crime) (getListAdapter()).getItem(position);
         Log.d(TAG, crime.getTitle() + " was clicked");
+    }
+
+    private class CrimeAdapter extends ArrayAdapter<Crime> {
+
+        public CrimeAdapter(ArrayList<Crime> crimes) {
+            super(getActivity(), 0, crimes);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // If we weren't given a view, inflate one
+            if (convertView == null) {
+                convertView = getActivity().getLayoutInflater()
+                        .inflate(R.layout.list_item_crime, null);
+            }
+
+            // Configure the view for this Crime
+            Crime crime = getItem(position);
+
+            TextView titleTextView = (TextView) convertView.findViewById(R.id.crime_list_item_titleTextView);
+            titleTextView.setText(crime.getTitle());
+
+            TextView dateTextView = (TextView) convertView.findViewById(R.id.crime_list_item_dateTextView);
+            dateTextView.setText(crime.getDate().toString());
+
+            CheckBox solvedCheckBox = (CheckBox)convertView.findViewById(R.id.crime_list_item_solvedCheckBox);
+            solvedCheckBox.setChecked(crime.isSolved());
+
+            return convertView;
+        }
     }
 }
