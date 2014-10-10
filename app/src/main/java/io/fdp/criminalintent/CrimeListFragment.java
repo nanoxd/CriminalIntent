640802e19c1206e20868db1cfd7d6e29a7d418d7
Fernando Paredes <nano@fdp.io>
2014-10-09 18:50:14 -0700

    Use ArrayAdapter to list crimes

--- app/src/main/java/io/fdp/criminalintent/CrimeListFragment.java
+++ app/src/main/java/io/fdp/criminalintent/CrimeListFragment.java
@@ -2,6 +2,7 @@ package io.fdp.criminalintent;
 
 import android.app.ListFragment;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 
@@ -17,5 +18,9 @@ public class CrimeListFragment extends ListFragment {
         super.onCreate(savedInstanceState);
         getActivity().setTitle(R.string.crimes_title);
         mCrimes = CrimeLab.get(getActivity()).getCrimes();
+
+        ArrayAdapter<Crime> adapter = new ArrayAdapter<Crime>(getActivity(),
+                android.R.layout.simple_list_item_1,  mCrimes);
+        setListAdapter(adapter);
     }
 }
